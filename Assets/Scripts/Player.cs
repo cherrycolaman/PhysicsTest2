e1396872b2c54ba178f2c7293a7a405e60883c3e
cherrycolaman <148461991+cherrycolaman@users.noreply.github.com>
2024-11-16 12:40:33 -0500

    Circle moves with WASD

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Vector2 direction;
+    public Rigidbody2D rb;
+    public float force = 200;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        rb.AddForce(direction * force * Time.deltaTime);
     }
 }
